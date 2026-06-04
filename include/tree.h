e8a13d9b8732be54cce68135ceb88b8cf8395fb7
shwannhello <shvarovaann@yandex.ru>
2026-06-05 02:03:01 +0300

    Update tree.h 6

--- include/tree.h
+++ include/tree.h
@@ -1,45 +1,45 @@
-// include/tree.h
-// Copyright 2024 <Student>
-
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
-#include <cstddef>
-#include <memory>
 #include <vector>
+#include <memory>
+#include <algorithm>
+
+class TreeNode {
+public:
+    char value;
+    std::vector<std::shared_ptr<TreeNode>> children;
+    
+    TreeNode(char val) : value(val) {}
+};
 
 class PMTree {
- private:
-  struct Node {
-    char value;
-    std::vector<std::shared_ptr<Node>> children;
-
-    explicit Node(char val) : value(val) {}
-  };
-
-  std::shared_ptr<Node> root;
-  std::vector<char> originalElements;
-
-  void buildTree(std::shared_ptr<Node> node, std::vector<char> remaining);
-  void collectPerms(std::shared_ptr<Node> node, std::vector<char>& current,
-                    std::vector<std::vector<char>>& result);
-  void collectPermsSequential(std::shared_ptr<Node> node,
-                              std::vector<char>& current, int& counter,
-                              int target, std::vector<char>& targetPerm,
-                              bool& found);
-  void findPermByPath(std::shared_ptr<Node> node, std::vector<char>& current,
-                      int& remaining, int target, std::vector<char>& result,
-                      bool& found);
-
- public:
-  explicit PMTree(const std::vector<char>& elements);
-
-  friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
-  friend std::vector<char> getPerm1(PMTree& tree, int num);
-  friend std::vector<char> getPerm2(PMTree& tree, int num);
-
-  int getTotalPermsCount();
-  std::shared_ptr<Node> getRoot() const { return root; }
+private:
+    std::shared_ptr<TreeNode> root;
+    std::vector<char> originalElements;
+    
+    void buildTree(std::shared_ptr<TreeNode> node, std::vector<char> remaining);
+    void getAllPermsRecursive(std::shared_ptr<TreeNode> node, 
+                              std::vector<char>& current, 
+                              std::vector<std::vector<char>>& result);
+    
+public:
+    PMTree(const std::vector<char>& elements);
+    ~PMTree() = default;
+    
+    std::shared_ptr<TreeNode> getRoot() const { return root; }
+    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+    friend std::vector<char> getPerm1(PMTree& tree, int num);
+    friend std::vector<char> getPerm2(PMTree& tree, int num);
 };
+
+// Функции для работы с деревом
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
+// Вспомогательная функция для вычисления факториала
+int factorial(int n);
 
 #endif  // INCLUDE_TREE_H_
