83967b219f4bd981139f00edb5f54111c6c844d8
shwannhello <shvarovaann@yandex.ru>
2026-06-05 01:29:13 +0300

    Update main.h

--- include/main.h
+++ include/main.h
@@ -1,8 +1,7 @@
+// include/main.h
 // Copyright 2024 <Student>
 
 #ifndef INCLUDE_MAIN_H_
 #define INCLUDE_MAIN_H_
-
-// Пустой файл для удовлетворения требований cpplint
 
 #endif  // INCLUDE_MAIN_H_
